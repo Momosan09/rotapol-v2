fe96706c589697b9cb9a7306a144f41a6c3b36b6
Gael Paez <gaelpaezg@gmail.com>
2021-12-16 17:53:16 -0300

    Imprimir v2

--- client/src/Detalles.js
+++ client/src/Detalles.js
@@ -14,13 +14,25 @@ const Detalles = props => {
       .catch(err => console.log(err));
   }, [])
 
+  const print = () => {
+    document.getElementById('print').hidden = true;
+    document.getElementById('volver').hidden = true;
+
+    window.print();
+    document.getElementById('print').hidden = false;
+    document.getElementById('volver').hidden = false;
+
+  };
+console.log(data.entregado);
   return (
     <>
     <h1>Cliente {params.id}</h1>
     <h2>Nombre: {data.name} {data.lastName}</h2>
     <h2>Telefono: {data.phone}</h2>
     <h2>Total: {data.precTotal}</h2>
-    <button onClick={() => window.print()}>Imprimir</button>
+    <h1>Estado: {data.entregado ? 'Entregado' : 'No Entregado'}</h1>
+    <button id="print" onClick={print}>Imprimir</button>
+    <a id="volver" href={"/admin"}>Volver</a>
     </>
   );
 };
