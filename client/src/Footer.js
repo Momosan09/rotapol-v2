ca7192a94c462f601a48de39edb9d96c54c71d41
Gael <momosama4585@gmail.com>
2022-01-12 13:25:27 -0300

    La master anda normal

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,15 +1,35 @@
+import Form2Comprar from './Form2comprar';
 /* import { Document, Page } from 'react-pdf' */
-const Footer = () => 
+const Footer = props => {
+
+    const { hayPresupuesto, modelo, cantTela, precTotal } = props;
 
 
+
+return(
 
     <>
+
+    <section className="footersection1">
+    <Form2Comprar
+    hayPresupuesto={hayPresupuesto}
+    modelo={modelo}
+    cantTela={cantTela}
+    precTotal={precTotal}
+    />
+
+    </section>
+
+
     <section className="footersection2">
     <h1>footer</h1>
     </section>
 
     </>
 
+);
 
 
+
+};
 export default Footer;
